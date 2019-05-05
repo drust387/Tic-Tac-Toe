ff67135bd39902fea84673c38773893b680bf0f9
drust387 <32627271+drust387@users.noreply.github.com>
2019-05-04 21:12:29 -0500

    Initial Entry

---
+++ js.js
@@ -0,0 +1,18 @@
+let sq1 = document.getElementById('sq1');
+let sq2 = document.getElementById('sq2');
+let sq3 = document.getElementById('sq3');
+let sq4 = document.getElementById('sq4');
+let sq5 = document.getElementById('sq5');
+let sq6 = document.getElementById('sq6');
+let sq7 = document.getElementById('sq7');
+let sq8 = document.getElementById('sq8');
+let sq9 = document.getElementById('sq9');
+
+if (sq1.innerHTML ==='X' || sq1.innerHTML ==='O') {
+    return true; 
+} else {
+    return false; 
+
+    
+
+    
