b429064ee22df1b19fdf90808891868f81397d49
drust387 <32627271+drust387@users.noreply.github.com>
2019-05-14 22:20:33 -0500

    Update js.js

--- js.js
+++ js.js
@@ -11,17 +11,20 @@ let sq9 = document.getElementById('sq9');
 let gameboard = [sq1,sq2,sq3,sq4,sq5,sq6,sq7,sq8,sq9];
 let winCombos = [[sq1,sq2,sq3],[sq4,sq5,sq6],[sq7,sq8,sq9],[sq1,sq4,sq7],[sq2,sq5,sq8],[sq3,sq6,sq9],[sq1,sq5,sq9],[sq7,sq5,sq3]];
 
-let movecount = 0; 
+let movecount = 0;
+ 
 
 
 let compPlay = () => {
-    console.log('started');
     gameboard.forEach(square => {
-            if (square.innerHTML === true) {
+                console.log('here I am');
+            if (square.innerHTML === 'X' || square.innerHTML === 'O') {
                 gameboard.splice(gameboard.indexOf(square),1) 
+                console.log(gameboard);
+                console.log('here I am 2');
             }
         })
-    
+        let winCombos = [[sq1,sq2,sq3],[sq4,sq5,sq6],[sq7,sq8,sq9],[sq1,sq4,sq7],[sq2,sq5,sq8],[sq3,sq6,sq9],[sq1,sq5,sq9],[sq7,sq5,sq3]];
     if(movecount === 1) {
         if(sq5.innerHTML === 'X') {
             compMoveViewable(sq7);
@@ -30,28 +33,30 @@ let compPlay = () => {
         }
 
     }else if(movecount > 1) {
-        console.log("I'm in here working");
         for (i=0; i < winCombos.length; i++) {
-            console.log("I'm in the loop!")
             let xCount = 0; 
             let oCount = 0;  
             let totCount = 0; 
                 winCombos[i].forEach(value => {
-                    console.log(value);
                     if(value.innerHTML === 'X') {
-                    xCount += 1; 
+                    xCount +=1; 
                     totCount +=1; 
-                    console.log(oCount);
+                    
                     }else if (value.innerHTML ==='O') {
                     oCount += 1; 
                     totCount +=1; 
-                    console.log(xCount);
-                    } else if (xCount === 2 || oCount === 2 && totCount < 3) {
-                        if (value.innerHTML !== 'X' && value.innerHTML !== 'O') {
-                            compMoveViewable(value);        
-                        } else {
-                            let nextMoveTwo = uniqueCompPlay();
-                            compMoveViewable(nextMoveTwo);
+                    
+                    } else if (value.innerHTML === '') {
+                        if (xCount === 2 && totCount < 3) {
+                            compMoveViewable(value); 
+                            break;       
+                        } else if (oCount === 2 && totCount < 3) {
+                            compMoveViewable(value);
+                            break; 
+                                }else {
+                                    let nextMoveTwo = uniqueCompPlay();
+                                    compMoveViewable(nextMoveTwo);
+                                    
                         }
                     }
             })
@@ -60,7 +65,7 @@ let compPlay = () => {
 }
 
 let uniqueCompPlay = () => {
-    return gameboard[Math.floor(Math.random() * gameboard.length)]
+    return gameboard[Math.floor(Math.random() * gameboard.length - 1)]
 } 
 
 let compMoveViewable = (square) => {
@@ -101,4 +106,3 @@ $(document).ready(() => {
     
 
 }) //jquery^^
-
