3451caf774b0464398949fbe87f8d5008ffbe0ab
drust387 <32627271+drust387@users.noreply.github.com>
2019-05-21 22:14:45 -0500

    Update js.js

--- js.js
+++ js.js
@@ -8,22 +8,22 @@ let sq7 = document.getElementById('sq7');
 let sq8 = document.getElementById('sq8');
 let sq9 = document.getElementById('sq9');
 
-let gameboard = [sq1,sq2,sq3,sq4,sq5,sq6,sq7,sq8,sq9];
-let winCombos = [[sq1,sq2,sq3],[sq4,sq5,sq6],[sq7,sq8,sq9],[sq1,sq4,sq7],[sq2,sq5,sq8],[sq3,sq6,sq9],[sq1,sq5,sq9],[sq7,sq5,sq3]];
+let gameboard = [sq1,sq2,sq3,sq4,sq5,sq6,sq7,sq8,sq9]; // array of all game board squares. 
+let winCombos = [[sq1,sq2,sq3],[sq4,sq5,sq6],[sq7,sq8,sq9],[sq1,sq4,sq7],[sq2,sq5,sq8],[sq3,sq6,sq9],[sq1,sq5,sq9],[sq7,sq5,sq3]]; //array of possible winning combinations
 
-let movecount = 0;
+let movecount = 0; // count of how many moves have been played. 
  
 
-
-let compPlay = () => {
+// compPlay is the function that decides which square the computer will play. 
+let compPlay = () => {      
     gameboard.forEach(square => {
             
             if (square.innerHTML === 'X' || square.innerHTML === 'O') {
-                gameboard.splice(gameboard.indexOf(square),1) 
+                gameboard.splice(gameboard.indexOf(square),1) // reduces gameboards so only unplayed squares remain in this array. 
                 
             }
         })
-        
+       // the if statement below decides where the first computer move should go based on the first user move. The computer move will either play the center square or square 7
             if(movecount === 1) {
                 if(sq5.innerHTML === 'X') {
                  compMoveViewable(sq7);
@@ -32,9 +32,10 @@ let compPlay = () => {
                 compMoveViewable(sq5);
                 console.log('Center wasnt played, so Im taking the obvious choice');
                 }
-
+     // if the first two moves have already been played we move into the second portion of the game. 
+      // the computer move should be decided by iterating through wincombos and playing any empty square that is in 
+         // the middle of a possible win combo that is almost filled by either X or O. 
             }else {
-                console.log('The movecount is higher than 1 so iterate through win combs to find best spot');
                 for (i=0; i < winCombos.length; i++) {
                  console.log('i =' + i);
                  let xCount = 0; 
@@ -44,21 +45,11 @@ let compPlay = () => {
                         console.log(value);
                         if(value.innerHTML === 'X') {
                              xCount +=1; 
-                             totCount +=1; 
-                             console.log('xCount =' + xCount);
-                             console.log('totCount =' + totCount);
-                             console.log(value);
-                             console.log(value.id);
-                             
-
+                             totCount += 1;     
                     
                          } else if (value.innerHTML ==='O') {
                              oCount += 1; 
                              totCount +=1;
-                             console.log('oCount =' + oCount);
-                             console.log('totCount =' + totCount); 
-                             console.log(value);
-                             console.log(value.id);
                     
                          } else if (value.innerHTML === '' && xCount === 2 && totCount < 3) {
                             console.log('Im playing defense');
